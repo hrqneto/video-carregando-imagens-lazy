c41d276a177e4ca04ac5b05ab99b7a6421714e5a
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2019-04-24 21:30:08 -0300

    Subindo projeto para Github

---
+++ assets/js/lazy.js
@@ -0,0 +1,14 @@
+(async () => {
+    if ('loading' in HTMLImageElement.prototype) {
+        const images = document.querySelectorAll("img.js-lazy");
+        images.forEach(img => img.src = img.dataset.src)
+    } else {
+        const lazySizesLib = await import('./lazysizes.min.js');
+        window.lazySizesConfig = window.lazySizesConfig || {}
+        window.lazySizesConfig.lazyClass = 'js-lazy'
+        window.lazySizesConfig.loadMode = 1
+        window.lazySizesConfig.loadingClass = 'is-loading'
+        window.lazySizesConfig.loadedClass = 'is-loaded'
+        lazySizes.init();
+    }
+})();
